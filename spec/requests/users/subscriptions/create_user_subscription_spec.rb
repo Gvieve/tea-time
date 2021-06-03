676a5f1f1e8138dbec9d46044d6db9af9e8391c4
Gvieve <gnuebel13@gmail.com>
2021-06-02 23:12:23 -0600

    Add sad path and edge case for user subscription

--- spec/requests/users/subscriptions/create_user_subscription_spec.rb
+++ spec/requests/users/subscriptions/create_user_subscription_spec.rb
@@ -6,7 +6,6 @@ describe 'User subscriptions request' do
       @user = create(:user)
       plan = create(:plan, weekly_frequency: 4)
       @teas = create_list(:tea, 3)
-      # user_profile = create(:user_profile, user: @user)
     end
     it "creates a user subscription and tea subscriptions when given valid data" do
       params = {
@@ -37,6 +36,64 @@ describe 'User subscriptions request' do
       check_hash_structure(json[:data][:attributes], :teas, Array)
       expect(json[:data][:attributes][:teas].count).to eq(2)
       expect(json[:data][:attributes][:teas].first).to be_a(Hash)
+
+      tea = json[:data][:attributes][:teas].first
+
+      check_hash_structure(tea, :tea_id, Integer)
+      check_hash_structure(tea, :title, String)
+      check_hash_structure(tea, :box_count, Integer)
+      check_hash_structure(tea, :quantity, Integer)
+      check_hash_structure(tea, :unit_price, String)
+      check_hash_structure(tea, :total_price, String)
+    end
+  end
+
+  describe 'sad path/edge cases' do
+    describe 'returns an error' do
+      before :all do
+        @user = create(:user)
+        plan = create(:plan, weekly_frequency: 4)
+        @teas = create_list(:tea, 3)
+      end
+      it "does not create a subscription when the user id is invalid" do
+        params = {
+          name: "My Monthly Tea Fix",
+          process_on_date: "2021-07-01",
+          weekly_frequency: 4,
+          teas: [ {tea_id: @teas.first.id, quantity: 1},
+                  {tea_id: @teas.second.id, quantity: 2} ]
+                }
+        header = {"CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json"}
+        id = 12345678
+        post "/api/v1/users/#{id}/subscriptions", headers: header, params: params.to_json
+
+        expect(response).to_not be_successful
+        json = JSON.parse(response.body, symbolize_names:true)
+
+        expect(response.status).to eq(404)
+        expect(json[:error]).to be_a(String)
+        expect(json[:error]).to eq("Couldn't find User with 'id'=#{id}")
+      end
+
+      it "does not create a subscription when required parameters are missing" do
+        params = {
+          name: "My Monthly Tea Fix",
+          weekly_frequency: 4,
+          teas: [ {tea_id: @teas.first.id, quantity: 1},
+                  {tea_id: @teas.second.id, quantity: 2} ]
+                }
+        header = {"CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json"}
+
+        post "/api/v1/users/#{@user.id}/subscriptions", headers: header, params: params.to_json
+
+        expect(response).to_not be_successful
+        json = JSON.parse(response.body, symbolize_names:true)
+
+        expect(response.status).to eq(400)
+        expect(json[:error]).to be_a(String)
+        expect(json[:error]).to eq("Required parameter missing")
+        expect(json[:url]).to be_a(String)
+      end
     end
   end
 end
